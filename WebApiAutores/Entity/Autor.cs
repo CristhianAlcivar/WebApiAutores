20096f690360616ddd87a37bb2f020dd9ca953e7
CristhianAlcivar <alcivarcristian@hotmail.com>
2022-10-26 20:36:49 -0500

    api autores

---
+++ WebApiAutores/Entity/Autor.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApiAutores.Entity
+{
+    public class Autor
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public List<Libro> Libros { get; set; }
+    }
+}
